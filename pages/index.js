d414feda403819c9a7c0ca4a670caff798ead6f2
Nexxer <yairopb@yandex.ru>
2021-04-22 21:50:11 +0300

    add index

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,19 @@ import style from './../styles/index.module.sass'
 const Index = () => {
 
   return (
-    <div>
-
+    <div className={style.page}>
+      <h1 className={style.title}>Тестовое задание FreeStylo</h1>
+      <hr/>
+      <a href="/favorites" className={style.link}>Избранное</a>
+      <div>
+        <form className={style.form}>
+          <label className={style.form__label}>Введите название канала</label>
+          <input placeholder={'Например: dota2mc_ru'} className={style.form__input} type={'text'} required={true}/>
+          <button type={"submit"} className={style.form__button}>Найти</button>
+        </form>
+      </div>
     </div>
-    );
+  );
 };
 
 export default Index;
